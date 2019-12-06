e353fee85ec184de93f6fbbbc7d862ec798c72e4
mmeshcher <keyvrn12012014@icloud.com>
2019-12-06 20:59:21 +0300

    first

---
+++ sources/main.cpp
@@ -0,0 +1,8 @@
+//Copyright 2019 <LinkIvan333>
+#include <Runner.hpp>
+int main() {
+    vector <int> cache_size;
+    Runner::cache_levels(cache_size);
+    Runner::run(cache_size);
+    return 0;
+}
